aa48c361e54cee27131fe6fac0f016a5d989d5f1
eles-pc <eleswastaken@gmail.com>
2021-06-05 18:52:49 +0600

    Add Comments; Fix Moves

--- main.py
+++ main.py
@@ -16,30 +16,43 @@ column = 0
 color = "grey"
 color_len = 0
 button_id = 0
-move = "black"
+turn = "black"
 
 
 ### ------------------------ BRAIN ------------------------------ ###
 
 def check_win():
-	
+	# if one of opponents runs out of tiles, one has lost
+	if len(whites) == 0 or len(blacks) == 0:
+		if blacks > whites:
+			print("BLACK WINS")
+		elif whites > blacks:
+			print("WHITE WINS")
+		for btn in all_buttons:
+			btn["state"] = "disabled"
+
 
 def check_move():
+	# two lines below make sure there's no disabled tiles left
 	for button in all_buttons:
 		button["state"] = "normal"
-	if move == "white" and  to_move != True:
+
+	if turn == "white" and  to_move != True:
 		for button in white_sq:
 			button["state"] = 'disabled'
 		for button in black_sq:
 			button["state"] = 'normal'
-	elif move == "black" and to_move != True:
+	elif turn == "black" and to_move != True:
 		for button in black_sq:
 			button["state"] = 'disabled'
 		for button in white_sq:
 			button["state"] = 'normal'
 
+	# being called every time a button is pressed
+	check_win()
+
 def eat(word, button_id):
-	global move
+	global turn
 	print("ate", word)
 	for dif in [-14,-18,14,18]:
 		if dif+last_bt_id==button_id:
@@ -58,15 +71,16 @@ def eat(word, button_id):
 		blacks.remove(eat_id)
 		black_sq.remove(eat_bt)
 	eat_bt.config(image=pixelVirtual)
-	if move == "white":
-		move = "black"
-	elif move == "black":
-		move = "white"
+	if turn == "white":
+		turn = "black"
+	elif turn == "black":
+		turn = "white"
 
 def make_move(button_id):
-	global move
+	global turn
 	print(button_id, 'made a move')
 
+	# this function removes-adds tiles-ids ~ registeres a move
 	def move_re():
 		if last_bt_id in whites:
 			button.config(image=white)
@@ -85,15 +99,27 @@ def make_move(button_id):
 		
 	button = all_buttons[button_id]
 	if (last_bt_id - button_id)%7==0 or (last_bt_id - button_id)%9==0:
+		# moving only one tile
 		for dif in [-7,-9,7,9]:
+			# checking wether selected tile is withing 1 tile
 			if last_bt_id+dif == button_id:
-				move_re()
-				print('hi hi')
-				# implement here no-go-back too
-				if move == "white":
-					move = "black"
-				elif move == "black":
-					move = "white"
+				# checking for white tile to go only forward
+				if last_bt_id in whites and button_id > last_bt_id:
+					move_re()
+					if turn == "white":
+						turn = "black"
+					elif turn == "black":
+						turn = "white"
+				# checking for black tile to go only forward
+				elif last_bt_id in blacks and button_id < last_bt_id:
+					move_re()
+					if turn == "white":
+						turn = "black"
+					elif turn == "black":
+						turn = "white"
+				else:
+					print("You cannot go back.")
+		# eating opponent's one tile and making a move
 		else:
 			for dif in [-14,-18,14,18]:
 				if last_bt_id+dif == button_id:
@@ -103,77 +129,24 @@ def make_move(button_id):
 					elif dif/2+last_bt_id in blacks:
 						eat("black", button_id)
 						move_re()
-					print('hi twice')
 			else:
 				print('too far')
 		
 	else:
 		print("out of line")
-		# for dif in [-14,-18,14,18]:
-		# 	if last_bt_id+dif == button_id:
-		# 		if dif/2+last_bt_id in blacks or dif/2+last_bt_id in whites:
-					
-		# 			if last_bt in black_sq and (last_bt_id-9 in whites or last_bt_id-7 in whites or last_bt_id+9 in whites or last_bt_id+7 in whites):
-		# 				eat("white", button_id)
-		# 			elif last_bt in white_sq and (last_bt_id-9 in blacks or last_bt_id-7 in blacks or last_bt_id+9 in blacks or last_bt_id+7 in blacks):
-		# 				eat("black", button_id)
-				
-		# 			if last_bt_id in whites:
-		# 				button.config(image=white)
-		# 				whites.append(button_id)
-		# 				whites.remove(last_bt_id)
-		# 				white_sq.append(button)
-		# 				white_sq.remove(last_bt)
-
-		# 			elif last_bt_id in blacks:
-		# 				button.config(image=black)
-		# 				blacks.append(button_id)
-		# 				blacks.remove(last_bt_id)
-		# 				black_sq.append(button)
-		# 				black_sq.remove(last_bt)
-		# 			last_bt.config(image=pixelVirtual)
-		# 			if move == "white":
-		# 				move = "black"
-		# 			elif move == "black":
-		# 				move = "white"
-		# 		else:
-					# print('too far')
-		# else:
-		# 	if button_id 
-		# 	if last_bt_id in whites:
-		# 		button.config(image=white)
-		# 		whites.append(button_id)
-		# 		whites.remove(last_bt_id)
-		# 		white_sq.append(button)
-		# 		white_sq.remove(last_bt)
-
-		# 	elif last_bt_id in blacks:
-		# 		button.config(image=black)
-		# 		blacks.append(button_id)
-		# 		blacks.remove(last_bt_id)
-		# 		black_sq.append(button)
-		# 		black_sq.remove(last_bt)
-		# 	last_bt.config(image=pixelVirtual)
-		# 	if move == "white":
-		# 		move = "black"
-		# 	elif move == "black":
-		# 		move = "white"
-	
-	
-	
 
 last_bt = None
 last_bt_id = None
 to_move = False
 def pressed(button_id):
 	global to_move, last_bt, last_bt_id
-	print(button_id, "got pressed")
 
+	# checks whether any button is on hold
 	if to_move:
 		to_move = False
 
 		if button_id in whites or button_id in blacks:
-			print('is not empty')
+			print('Is not empty.')
 		else:
 			make_move(button_id)
 		last_bt.config(bg='grey')
@@ -181,9 +154,11 @@ def pressed(button_id):
 	else:
 		to_move = True
 
+		# if empty tile is selected change the turn back (reselect)
 		if button_id not in whites and button_id not in blacks:
 			to_move = False
-
+		
+		# "selects button"/ turns tile with button_id in blue color
 		elif button_id in whites:			
 			button = white_sq[whites.index(button_id)]
 			button.config(bg='blue')
@@ -192,10 +167,13 @@ def pressed(button_id):
 			button = black_sq[blacks.index(button_id)]
 			button.config(bg='blue')
 		
+		# in case of epmty tile
 		try:
 			last_bt = button
 			last_bt_id = button_id
 		except: print('no peice')
+
+	# is being called every press, checks whose turn is
 	check_move()
 
 
@@ -281,7 +259,13 @@ for i in range(8):
 	column = 0
 	row +=1
 
-# move = Label(text="white to move")	
-# move.grid(column=0, row=row+1, columnspan=8)
+	## code below turns board 90 degrees
+	# 	row +=1
+	# 	color_len +=1
+	# 	button_id +=1
+	# color_len -=1
+	# row = 0
+	# column +=1
+
 
 window.mainloop()
