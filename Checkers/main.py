8a21e3c39adaad943c3517e1cef33474d44b0990
eles-pc <eleswastaken@gmail.com>
2021-06-20 13:05:00 +0600

    Intial Release

--- Checkers/main.py
+++ Checkers/main.py
@@ -18,11 +18,15 @@ color_len = 0
 button_id = 0
 turn = "black"
 
+window = Tk()
+window.title('Checkmates')
+window.minsize(500,500)
+window.config(padx=20, pady=20)
 
 ### ------------------------ BRAIN ------------------------------ ###
 
 def check_win():
-	# if one of opponents runs out of tiles, one has lost
+	# if one of players runs out of tiles, one has lost
 	if len(whites) == 0 or len(blacks) == 0:
 		if blacks > whites:
 			print("BLACK WINS")
@@ -33,71 +37,70 @@ def check_win():
 
 
 def check_move():
-	# two lines below make sure there's no disabled tiles left
+	# two lines below make sure there are no disabled tiles left
 	for button in all_buttons:
 		button["state"] = "normal"
 
-	if turn == "white" and  to_move != True:
+	if turn == "white" and  on_hold != True:
 		for button in white_sq:
 			button["state"] = 'disabled'
 		for button in black_sq:
 			button["state"] = 'normal'
-	elif turn == "black" and to_move != True:
+	elif turn == "black" and on_hold != True:
 		for button in black_sq:
 			button["state"] = 'disabled'
 		for button in white_sq:
 			button["state"] = 'normal'
 
-	# being called every time a button is pressed
+	# --> called every move
 	check_win()
 
-def eat(word, button_id):
+def eat_tile(which, lst_bt_id, btn_id):
 	global turn
-	print("ate", word)
-	for dif in [-14,-18,14,18]:
-		if dif+last_bt_id==button_id:
-			if dif/2+last_bt_id in whites:
-				eat_id = dif/2+last_bt_id
-				eat_bt = white_sq[whites.index(eat_id)]
-				cl_bt = "white"
-			elif dif/2+last_bt_id in blacks:
-				eat_id = dif/2+last_bt_id
-				eat_bt = black_sq[blacks.index(eat_id)]
-				cl_bt = "black"
-	if word == "white" and cl_bt == "white":
-		whites.remove(eat_id)
-		white_sq.remove(eat_bt)	
-	elif word =="black" and cl_bt == "black":
-		blacks.remove(eat_id)
-		black_sq.remove(eat_bt)
-	eat_bt.config(image=pixelVirtual)
+	for step in [-14,-18,14,18]:
+		# checks whether between last-bt-id and bt-id is a tile
+		if which == "black" and step/2+lst_bt_id in whites:
+			white_sq[whites.index(step/2+lst_bt_id)].config(image=pixelVirtual)
+			white_sq.remove(white_sq[whites.index(step/2+lst_bt_id)])
+			whites.remove(step/2+lst_bt_id)
+			move_re(lst_bt_id, btn_id)
+
+		elif which == "white" and step/2+lst_bt_id in blacks:
+			black_sq[blacks.index(step/2+lst_bt_id)].config(image=pixelVirtual)
+			black_sq.remove(black_sq[blacks.index(step/2+lst_bt_id)])
+			blacks.remove(step/2+lst_bt_id)
+			move_re(lst_bt_id, btn_id)
+		# for n_step in [-14,-18,14,18]:
+		# 	if (n_step/2+btn_id in blacks or n_step/2+btn_id in whites) and (btn_id+n_step not in whites) and (btn_id+n_step not in blacks):
+		# 		print("hello, this it a;fasddhdhfg")
+		# 		# check whether player wants
 	if turn == "white":
 		turn = "black"
 	elif turn == "black":
-		turn = "white"
+		turn = "white"		
+
+# this function removes-adds tiles-ids ~ registeres a move
+def move_re(lst_bt_id, btn_id):
+	button = all_buttons[btn_id]
+	lst_bt = all_buttons[lst_bt_id]
+	if lst_bt_id in whites:
+		button.config(image=white)
+		whites.append(btn_id)
+		whites.remove(lst_bt_id)
+		white_sq.append(button)
+		white_sq.remove(last_bt)
+
+	elif lst_bt_id in blacks:
+		button.config(image=black)
+		blacks.append(btn_id)
+		blacks.remove(lst_bt_id)
+		black_sq.append(button)
+		black_sq.remove(last_bt)
+	lst_bt.config(image=pixelVirtual)
 
 def make_move(button_id):
 	global turn
-	print(button_id, 'made a move')
 
-	# this function removes-adds tiles-ids ~ registeres a move
-	def move_re():
-		if last_bt_id in whites:
-			button.config(image=white)
-			whites.append(button_id)
-			whites.remove(last_bt_id)
-			white_sq.append(button)
-			white_sq.remove(last_bt)
-
-		elif last_bt_id in blacks:
-			button.config(image=black)
-			blacks.append(button_id)
-			blacks.remove(last_bt_id)
-			black_sq.append(button)
-			black_sq.remove(last_bt)
-		last_bt.config(image=pixelVirtual)
-		
-	button = all_buttons[button_id]
 	if (last_bt_id - button_id)%7==0 or (last_bt_id - button_id)%9==0:
 		# moving only one tile
 		for dif in [-7,-9,7,9]:
@@ -105,58 +108,52 @@ def make_move(button_id):
 			if last_bt_id+dif == button_id:
 				# checking for white tile to go only forward
 				if last_bt_id in whites and button_id > last_bt_id:
-					move_re()
+					move_re(last_bt_id, button_id)
 					if turn == "white":
 						turn = "black"
 					elif turn == "black":
 						turn = "white"
 				# checking for black tile to go only forward
 				elif last_bt_id in blacks and button_id < last_bt_id:
-					move_re()
+					move_re(last_bt_id, button_id)
 					if turn == "white":
 						turn = "black"
 					elif turn == "black":
 						turn = "white"
 				else:
 					print("You cannot go back.")
-		# eating opponent's one tile and making a move
-		else:
-			for dif in [-14,-18,14,18]:
-				if last_bt_id+dif == button_id:
-					if dif/2+last_bt_id in whites:
-						eat("white", button_id)
-						move_re()
-					elif dif/2+last_bt_id in blacks:
-						eat("black", button_id)
-						move_re()
-			else:
-				print('too far')
-		
+			# eating opponent's one tile and making a move
+			elif last_bt_id+dif in whites and last_bt_id+dif*2 == button_id:
+				eat_tile("black", last_bt_id, button_id)
+			elif last_bt_id+dif in blacks and last_bt_id+dif*2 == button_id:
+				eat_tile("white", last_bt_id, button_id)
 	else:
 		print("out of line")
 
+
 last_bt = None
 last_bt_id = None
-to_move = False
+on_hold = False
 def pressed(button_id):
-	global to_move, last_bt, last_bt_id
+	global on_hold, last_bt, last_bt_id
 
 	# checks whether any button is on hold
-	if to_move:
-		to_move = False
+	if on_hold:
+		on_hold = False
 
 		if button_id in whites or button_id in blacks:
+			last_bt.config(bg='grey')
 			print('Is not empty.')
 		else:
 			make_move(button_id)
-		last_bt.config(bg='grey')
+			last_bt.config(bg='grey')
 
 	else:
-		to_move = True
+		on_hold = True
 
 		# if empty tile is selected change the turn back (reselect)
 		if button_id not in whites and button_id not in blacks:
-			to_move = False
+			on_hold = False
 		
 		# "selects button"/ turns tile with button_id in blue color
 		elif button_id in whites:			
@@ -180,10 +177,7 @@ def pressed(button_id):
 
 ### -------------------------- GUI ------------------------------ ###
 
-window = Tk()
-window.title('Checkmates')
-window.minsize(500,500)
-window.config(padx=20, pady=20)
+
 
 black = PhotoImage(file="images/black.png")
 white = PhotoImage(file="images/white.png")
@@ -266,6 +260,5 @@ for i in range(8):
 	# color_len -=1
 	# row = 0
 	# column +=1
-
 
 window.mainloop()
